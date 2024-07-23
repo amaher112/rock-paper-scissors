fcd564315cf8666dbc5e020ab15054464286c8df
amaher <amaher112@su.edu>
2024-07-23 18:15:33 -0400

    added scoring if statements

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const Rock = document.getElementById('Rock')
-const Paper = document.getElementById('Paper')
-const Scissors = document.getElementById('Scissors')
+const Rock = document.getElementById("Rock");
+const Paper = document.getElementById("Paper");
+const Scissors = document.getElementById("Scissors");
 const compChoice = document.querySelector(".comp-choice");
-const you = document.getElementById('you')
-const computer = document.getElementById('computer')
-const ties = document.getElementById('ties')
+const you = document.getElementById("you");
+const computer = document.getElementById("computer");
+const ties = document.getElementById("ties");
 
 // attach event listener to each button
 // event listener will prompt the computer to pick rock, paper or scissors at random
@@ -13,9 +13,7 @@ let youScore = 0;
 let computerScore = 0;
 let tiesScore = 0;
 
-you.textContent = "You: " + youScore;
-computer.textContent = "Computer: " + computerScore;
-ties.textContent = "Ties: " + tiesScore;
+
 
 function playGame() {
   const options = ["Rock", "Paper", "Scissors"];
@@ -24,20 +22,28 @@ function playGame() {
   const userChoice = this.id;
   console.log(computerChoice);
   compChoice.textContent = computerChoice;
-  console.log(userChoice)
+  console.log(userChoice);
 
   if (userChoice == computerChoice) {
-    tiesScore++
-    console.log(tiesScore)
+    tiesScore++;
+    ties.textContent = "Ties: " + tiesScore;
+    console.log(tiesScore);
+  } else if (
+    (userChoice === "Rock" && computerChoice === "Scissors") ||
+    (userChoice === "Paper" && computerChoice === "Rock") ||
+    (userChoice === "Scissors" && computerChoice === "Paper")
+  ) {
+    youScore++;
+    you.textContent = "You: " + youScore;
+  } else {
+    computerScore++;
+    computer.textContent = "Computer: " + computerScore;
   }
 }
 
 // it will then compare users choice with computers choice
 // score will be increased based on choices
 
-Rock.addEventListener('click', playGame)
-Paper.addEventListener('click', playGame)
-Scissors.addEventListener('click', playGame)
-
-
-
+Rock.addEventListener("click", playGame);
+Paper.addEventListener("click", playGame);
+Scissors.addEventListener("click", playGame);
